db78222519c0cae18c078983ebcd425824639705
O-Joy <joyce.lopes.1988@gmail.com>
2026-04-12 11:57:36 +0100

    definição do ficheiro com as constantes da app

---
+++ lib/utils/constants.dart
@@ -0,0 +1,22 @@
+// ficheiro para armazenamento de constantes -> todos os valores são static
+
+class AppConstants {
+  AppConstants._(); //construtor privado
+
+  static const String baseUrl =
+      'http://10.0.2.2:3000/api'; // URL base da API REST
+
+  static const String dbName = 'pint2526.db'; //base de dados local (SQlite)
+  static const int dbVersion =
+      1; //versão da db -> compara e gatante que o user tem a versão atual da db caso a altere
+
+  //chaves do SharedPreferences (tipo "cockies" com informações do consultor)
+  static const String keyToken =
+      'auth_token'; //dados de login (email e password incriptados)
+  static const String keyUserId = 'user_id'; //id do consultor
+  static const String keyUserName = 'user_name'; //nome
+  static const String keyUserArea = 'user_area'; //area escolhida pelo consultor
+
+  static const int diasAlertaExpiracao = 30; // dias restantes para alertar sobre a expiração de um badge
+
+}
