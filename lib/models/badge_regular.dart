14d98e5cf60e90442ff84fb5d70d102d1b088f90
O-Joy <joyce.lopes.1988@gmail.com>
2026-04-15 10:34:01 +0100

    models: BadgeRegular criado

---
+++ lib/models/badge_regular.dart
@@ -0,0 +1,64 @@
+class BadgeRegular {
+  final int id;
+  final String nome;
+  final String?
+  descricao; // colocar '?' nos valores que podem ser nulos -> ver script das tabelas
+  final int? pontos;
+  final String? urlImage;
+  final int idNivel;
+  final String nomeNivel;
+  final int idServiceLine;
+  final String nomeServiceLine;
+  final int idArea;
+  final String nomeArea;
+
+  //construtor
+  BadgeRegular({
+    required this.id, //colocar required nos campos obrigatórios
+    required this.nome,
+    this.descricao,
+    this.pontos,
+    this.urlImage,
+    required this.idNivel,
+    required this.nomeNivel,
+    required this.idServiceLine,
+    required this.nomeServiceLine,
+    required this.idArea,
+    required this.nomeArea,
+  });
+
+  //fromJson - converto do formato json da API para o objeto
+  //O método factory recebe o json convertido em map de strings pelo package http e traduz
+  factory BadgeRegular.fromJson(Map<String, dynamic> json) {
+    return BadgeRegular(
+      id: json['id'],
+      nome: json['nome'],
+      descricao: json['descricao'],
+      pontos: json['pontos'],
+      urlImage: json['urlImagem'],
+      idNivel: json['idNivel'],
+      nomeNivel: json['nomeNivel'],
+      idServiceLine: json['idServiceLine'],
+      nomeServiceLine: json['nomeServiceLine'],
+      idArea: json['idArea'],
+      nomeArea: json['nomeArea'],
+    );
+  }
+
+  //toJson - inverso do fromJson - converte o objecto em json (envia para a API também em map)
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'nome': nome,
+      'descricao': descricao,
+      'pontos': pontos,
+      'urlImagem': urlImage,
+      'idNivel': idNivel,
+      'nomeNivel': nomeNivel,
+      'idServiceLine': idServiceLine,
+      'nomeServiceLine': nomeServiceLine,
+      'idArea': idArea,
+      'nomeArea': nomeArea,
+    };
+  }
+}
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,19 +4,19 @@ class AppConstants {
   AppConstants._(); //construtor privado
 
   static const String baseUrl =
-      'http://10.0.2.2:3000/api'; // URL base da API REST
+      'http://10.0.2.2:3001/api'; // URL base da API REST
 
   static const String dbName = 'pint2526.db'; //base de dados local (SQlite)
   static const int dbVersion =
-      1; //versão da db -> compara e gatante que o user tem a versão atual da db caso a altere
+      1; //versão da db -> compara e garante que o user tem a versão atual da db caso a altere
 
-  //chaves do SharedPreferences (tipo "cockies" com informações do consultor)
+  //chaves do SharedPreferences (para aceder a informações guardadas em SharedPreferences)
   static const String keyToken =
       'auth_token'; //dados de login (email e password incriptados)
   static const String keyUserId = 'user_id'; //id do consultor
   static const String keyUserName = 'user_name'; //nome
   static const String keyUserArea = 'user_area'; //area escolhida pelo consultor
 
-  static const int diasAlertaExpiracao = 30; // dias restantes para alertar sobre a expiração de um badge
-
+  static const int diasAlertaExpiracao =
+      30; // dias restantes para alertar sobre a expiração de um badge
 }
