66fdef2e2b26d6a437d05159b6458b163a1894ab
Ana Chaves <chavesrita89@gmail.com>
2026-06-01 17:08:50 +0100

    Modificações: Rotas adicionei nova recuperar password modifiquei a rota refifinir password modifiquei a rota e os parâmetros do email Redefinir password 2: Modifiquei alguns aspetos para receber bem o email e pass, já testado tudo funcionar Candidatura definida: apenas a ppbar para estar em conformidade com o dashboard Candidaturas_decorrrentes: a mesma coisa Candidaturas: várias modificações, no entanto adicionei uma área de rascunhos de forma a pessoa conseguir voltar ao que estava e preencher, eu sei que é um extra mas fica mais interessante a app. Detalhes Candidaturas e histórico: A app bar outra vez Nova Candidatura: tive de fazer modificações de forma a associar o rascunho à mesma para o consultor conseguir continuar a preencher o rascunho de candidaturas Modifiquei a app bar de notificações e detalhes notificaçoes para estar em conformidade Na api adicionei métodos para os rascunhos

--- lib/screens/candidaturas/nova_candidatura_screen.dart
+++ lib/screens/candidaturas/nova_candidatura_screen.dart
@@ -130,8 +130,8 @@ class _NovaCandidaturaState extends State<NovaCandidatura> {
             onPressed: () => Scaffold.of(ctx).openDrawer(),
           ),
         ),
-        title: const Text('CANDIDATURAS',
-            style: TextStyle(color: AppConstants.corPrimaria, fontWeight: FontWeight.bold, fontSize: 15, letterSpacing: 1)),
+        title: const Text('Candidaturas',
+            style: TextStyle(color: AppConstants.corPrimaria, fontWeight: FontWeight.bold, fontSize: 20)),
         actions: [
           IconButton(
             icon: SvgPicture.asset('assets/icons/notificacoesprimaria.svg', height: 24,
